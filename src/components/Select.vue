4b25e890aea2c74a9c09f2b7cbc11ee78676cb97
Jean Le Clerc <jean@nibnut.com>
2017-09-01 16:40:24 -0400

    Added placeholder as hint option

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -954,6 +954,7 @@
        * @return {String} Placeholder text
        */
       searchPlaceholder() {
+        console.log(this.placeholderAsHint, "||", (this.isValueEmpty && this.placeholder))
         if (this.placeholderAsHint || (this.isValueEmpty && this.placeholder)) {
           return this.placeholder;
         }
